eb5fbed7548024d8bbea77f2a8d9582183b2c1ff
Mridul Gupta <83527720+Modularity-22@users.noreply.github.com>
2021-05-05 17:56:40 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -2,30 +2,30 @@ public class Q3
 {public static void main(String[] args) 
     {
 
-        for (int i = 1; i <= 7; i++) 
+        for (int i=1;i<=7;i++) 
         {
-            for (int j = 1; j < i; j++) 
+            for (int j=1;j<i;j++) 
             {
                 System.out.print(" ");
             }
              
-            for (int j = i; j <= 7; j++) 
+            for (int j=i;j<=7;j++) 
             { 
                 System.out.print(j+" "); 
             } 
              
             System.out.println(); 
         } 
-        for (int i = 7; i >= 1; i--) 
+        for (int i=7;i>=1;i--) 
         {
              
-            for (int j = 1; j < i; j++) 
+            for (int j=1;j<i;j++) 
             {
                 System.out.print(" ");
             }
 
              
-            for (int j = i; j <= 7; j++)
+            for (int j=i;j<=7;j++)
             {
                 System.out.print(j+" ");
             }
