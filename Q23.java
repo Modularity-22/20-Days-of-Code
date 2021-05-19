57d0c505b852517a08f8be0d83b4733ad8b9678d
Mridul Gupta <83527720+Modularity-22@users.noreply.github.com>
2021-05-19 21:33:36 +0530

    Day 16

---
+++ Q23.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Q23 {
+    public static void main(String[] args){
+        String var;
+        Scanner sc = new Scanner(System.in);
+        var = sc.next();
+        System.out.println(reverseOnlyLetters(var));
+
+    }
+    public static String reverseOnlyLetters(String S) {
+        StringBuilder ans = new StringBuilder();
+        int j = S.length() - 1;
+        for (int i = 0; i < S.length(); ++i) {
+            if (Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j)))
+                    j--;
+                ans.append(S.charAt(j--));
+            } else {
+                ans.append(S.charAt(i));
+            }
+        }
+
+        return ans.toString();
+    }
+}
